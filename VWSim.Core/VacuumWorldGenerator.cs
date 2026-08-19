0926efaf0d3ce57403b97c22d0bcab36afe8761d
arthandreyStudent <arthandrey.endrina@cit.edu>
2026-08-19 22:37:15 +0800

    feat: Add a world state saver and a world generator for the vacuum world environment.

---
+++ VWSim.Core/VacuumWorldGenerator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VWSim.Core
+{
+    public class VacuumWorldGenerator
+    {
+        private readonly Random _rand;
+
+        public VacuumWorldGenerator()
+        {
+            _rand = new Random();
+        }
+
+        public VacuumWorldState Generate()
+        {
+            int[,] grid = new int[2, 2];
+            List<Dirt> dirts = new List<Dirt>();
+
+            // Generate grid with random dirt positions
+            for (int row = 0; row < 2; row++)
+            {
+                for (int col = 0; col < 2; col++)
+                {
+                    grid[row, col] = _rand.Next(0, 2); // 0 for clean, 1 for dirty
+                }
+            }
+
+            // Generate Dirt objects
+            int dirtRandomOffset = 25;
+
+            for (int row = 0; row < 2; row++)
+            {
+                for (int col = 0; col < 2; col++)
+                {
+                    int offsetX = _rand.Next(-dirtRandomOffset, dirtRandomOffset + 1);
+                    int offsetY = _rand.Next(-dirtRandomOffset, dirtRandomOffset + 1);
+
+                    if (grid[row, col] == 1)
+                    {
+                        dirts.Add(new Dirt(row, col, offsetX, offsetY));
+                    }
+                }
+            }
+
+            return new VacuumWorldState(grid, dirts, 0, 0); // Agent starts at (0, 0)
+        }
+
+    }
+}
---
+++ VWSim.Core/VacuumWorldState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VWSim.Core
+{
+    public class VacuumWorldState
+    {
+
+        public int[,] Grid { get; }
+        public List<Dirt> Dirts { get; }
+
+        public int AgentX { get; }
+        public int AgentY { get; }
+
+        public VacuumWorldState(int[,] grid, List<Dirt> dirts, int agentX, int agentY)
+        {
+            Grid = grid;
+            Dirts = dirts;
+            AgentX = agentX;
+            AgentY = agentY;
+        }
+
+    }
+}
