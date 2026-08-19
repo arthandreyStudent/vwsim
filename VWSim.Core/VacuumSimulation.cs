0e0c85600e1c3b45dbb70ef8fa42abeacf8fa36f
arthandreyStudent <arthandrey.endrina@cit.edu>
2026-08-19 22:36:38 +0800

    feat: Add VacuumSimulation class to manage all agent simulations into one concurrent simulation.

---
+++ VWSim.Core/VacuumSimulation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VWSim.Core
+{
+    public class VacuumSimulation
+    {
+        private List<AgentSimulation> _agentSimulations;
+
+        public VacuumSimulation(List<AgentSimulation> agentSimulations)
+        {
+            _agentSimulations = agentSimulations;
+        }
+
+        public void RunStep()
+        {
+            foreach (var agentSimulation in _agentSimulations)
+            {
+                agentSimulation.RunStep();
+            }
+        }
+        
+    }
+}
