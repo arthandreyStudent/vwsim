411cb9ddc05bcafc16fac9e7fb7786b9377803ad
arthandreyStudent <arthandrey.endrina@cit.edu>
2026-08-20 01:03:20 +0800

    feat: Add a User Control (AgentSimulationPanel) for rendering the different agent simulation UI, making it reusable.

---
+++ VWSim/AgentSimulationPanel.Designer.cs
@@ -0,0 +1,94 @@
+﻿using VWSim;
+
+namespace VWSim
+{
+    partial class AgentSimulationPanel
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxVacuumWorld = new System.Windows.Forms.PictureBox();
+            this.richTextBoxLog = new System.Windows.Forms.RichTextBox();
+            this.labelAgentName = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxVacuumWorld)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxVacuumWorld
+            // 
+            this.pictureBoxVacuumWorld.BackColor = System.Drawing.Color.Black;
+            this.pictureBoxVacuumWorld.Location = new System.Drawing.Point(84, 89);
+            this.pictureBoxVacuumWorld.Name = "pictureBoxVacuumWorld";
+            this.pictureBoxVacuumWorld.Size = new System.Drawing.Size(250, 250);
+            this.pictureBoxVacuumWorld.TabIndex = 0;
+            this.pictureBoxVacuumWorld.TabStop = false;
+            // 
+            // richTextBoxLog
+            // 
+            this.richTextBoxLog.BackColor = System.Drawing.Color.Black;
+            this.richTextBoxLog.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBoxLog.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBoxLog.ForeColor = System.Drawing.Color.Lime;
+            this.richTextBoxLog.Location = new System.Drawing.Point(14, 354);
+            this.richTextBoxLog.Name = "richTextBoxLog";
+            this.richTextBoxLog.Size = new System.Drawing.Size(399, 484);
+            this.richTextBoxLog.TabIndex = 1;
+            this.richTextBoxLog.Text = "";
+            // 
+            // labelAgentName
+            // 
+            this.labelAgentName.AutoSize = true;
+            this.labelAgentName.Font = new System.Drawing.Font("Unispace", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAgentName.ForeColor = System.Drawing.Color.White;
+            this.labelAgentName.Location = new System.Drawing.Point(79, 35);
+            this.labelAgentName.Name = "labelAgentName";
+            this.labelAgentName.Size = new System.Drawing.Size(163, 29);
+            this.labelAgentName.TabIndex = 2;
+            this.labelAgentName.Text = "Agent Name";
+            this.labelAgentName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // AgentSimulationPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(49)))), ((int)(((byte)(49)))));
+            this.Controls.Add(this.labelAgentName);
+            this.Controls.Add(this.richTextBoxLog);
+            this.Controls.Add(this.pictureBoxVacuumWorld);
+            this.Name = "AgentSimulationPanel";
+            this.Size = new System.Drawing.Size(430, 861);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxVacuumWorld)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxVacuumWorld;
+        private System.Windows.Forms.RichTextBox richTextBoxLog;
+        private System.Windows.Forms.Label labelAgentName;
+    }
+}
---
+++ VWSim/AgentSimulationPanel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VWSim.Core;
+
+namespace VWSim
+{
+    public partial class AgentSimulationPanel : UserControl
+    {
+        private AgentSimulation _agentSimulation;
+        private VacuumWorldRenderer _renderer;
+
+        public AgentSimulationPanel()
+        {
+            InitializeComponent();
+
+            _renderer = new VacuumWorldRenderer(250, 125); // 2x2 grid, so each cell is 125x125
+        }
+
+        public void Initialize(AgentSimulation agentSimulation)
+        {
+            _agentSimulation = agentSimulation;
+            RenderAgentName(agentSimulation.Name);
+            ClearLog();
+            RenderEnvironment();
+            DisplayEnvironmentState();
+        }
+
+        public void RenderAgentName(string agentName)
+        {
+            labelAgentName.Text = agentName;
+        }
+
+        public void ClearLog()
+        {
+            richTextBoxLog.Clear();
+        }
+
+        public void DisplayEnvironmentState()
+        {
+            VacuumEnvironment environment = _agentSimulation.Environment;
+            Log(environment.ToString());
+        }
+
+        public void DisplayResult(SimulationStepResult result, int step)
+        {
+            var action = result.Action;
+            var agentLoc = result.AgentLoc;
+            var performance = result.AgentPerformance;
+
+            Log($"Step {step}: Action: {action} | Location: ({agentLoc[0]}, {agentLoc[1]}) | Score: {performance}");
+
+            RenderEnvironment();
+        }
+
+        public void RenderEnvironment()
+        {
+            Bitmap image = _renderer.Render(_agentSimulation.Environment);
+
+            if (pictureBoxVacuumWorld.Image != null)
+            {
+                pictureBoxVacuumWorld.Image.Dispose();
+            }
+
+            pictureBoxVacuumWorld.Image = image;
+        }
+
+        public void Log(string message)
+        {
+            richTextBoxLog.AppendText(message + System.Environment.NewLine);
+        }
+    }
+}
