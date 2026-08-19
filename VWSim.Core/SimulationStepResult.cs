02245afae8583c4b3d0fb3c2750cf40ef8c2f9ac
arthandreyStudent <arthandrey.endrina@cit.edu>
2026-08-20 01:00:20 +0800

    feat: Add SimulationStepResult class that returns the result of an agent's simulation

---
+++ VWSim.Core/SimulationStepResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VWSim.Core
+{
+    public class SimulationStepResult
+    {
+        public string Action { get; }
+        public int[] AgentLoc {  get; }
+        public int AgentPerformance { get; }
+
+        public SimulationStepResult(string action, int[] agentLoc, int agentPerformance)
+        {
+            Action = action;
+            AgentLoc = agentLoc;
+            AgentPerformance = agentPerformance;
+        }
+
+    }
+}
