140033b7073192638369375fd36e8e090d54f7fb
arthandreyStudent <arthandrey.endrina@cit.edu>
2026-08-19 18:38:39 +0800

    feat: Add SimpleReflextAgent

---
+++ VMSim/SimpleReflextAgent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VMSim
+{
+    public class SimpleReflextAgent : Agent
+    {
+
+        public override object Program(Tuple<int, int, bool> percept)
+        {
+            return null;
+        }
+
+    }
+}
